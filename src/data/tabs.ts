f8ec56f59bde8798bd3c3c3b6af5df54323a23ef
Damian Durnford <jw.roleyfam@gmail.com>
2026-09-11 16:29:42 +0800

    D

--- src/data/tabs.ts
+++ src/data/tabs.ts
@@ -2,20 +2,20 @@ export interface TabFile {
 	fileName: string;
 	title: string;
 	subTitle: string;
-    img: string;
+	img: string;
 }
 
 export const tabFiles: TabFile[] = [
 
 
-{ fileName: 'Bridge over still water.musicxml', title: 'Bridge over still water', subTitle: '', img: 'living-hope.png'},
+	{ fileName: 'Bridge over still water.musicxml', title: 'Bridge over still water', subTitle: '', img: 'living-hope.png' },
 
-{ fileName: 'Bridge Over Troubled Water.tab', title: 'Bridge Over Troubled Water', subTitle: 'Simon and Garfunkel', img: 'Simon_and_Garfunkel_1968.jpg'},
+	{ fileName: 'Bridge Over Troubled Water.tab', title: 'Bridge Over Troubled Water', subTitle: 'Simon and Garfunkel', img: 'Simon_and_Garfunkel_1968.jpg' },
 
-{ fileName: 'Get Back Honky Dog.musicxml', title: 'Get Back Honky Dog', subTitle: '', img: 'more-than-zero.png' },
+	{ fileName: 'Get Back Honky Dog.musicxml', title: 'Get Back Honky Dog', subTitle: '', img: 'more-than-zero.png' },
 
-{ fileName: 'Music Woman.musicxml', title: 'Music Woman' , subTitle: '', img: 'cinderella-in-the-rain.png'},
+	{ fileName: 'Music Woman.musicxml', title: 'Music Woman', subTitle: '', img: 'cinderella-in-the-rain.png' },
 
-
+	{ fileName: 'Just the Way You Are.tab', title: 'Just the Way You Are', subTitle: 'Billy Joel', img: 'Billyjoelpic1.webp' },
 
 ];
