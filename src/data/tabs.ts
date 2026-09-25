ecb48d91ef65ff7a1c5a81474aa1bd3bdb083d11
Damian Durnford <jw.roleyfam@gmail.com>
2026-09-25 22:47:21 +0800

    D

--- src/data/tabs.ts
+++ src/data/tabs.ts
@@ -9,7 +9,7 @@ export const tabFiles: TabFile[] = [
 
 	{ fileName: 'Bridge Over Troubled Water.tab', title: 'Bridge Over Troubled Water', subTitle: 'Simon and Garfunkel', img: 'Simon_and_Garfunkel_1968.jpg' },
 
-	{ fileName: 'Just the Way You Are.tab', title: 'Just the Way You Are', subTitle: 'Billy Joel', img: 'Billyjoelpic1.webp' },
+	{ fileName: 'Just the Way You Are.tab', title: 'Just the Way You Are', subTitle: 'Billy Joel', img: 'BillyJoel.webp' },
 
 ];
 
